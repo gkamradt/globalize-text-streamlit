d5da91003e56050063c831bafee48bc61c84f793
Greg Kamradt <gkamradt@gmail.com>
2023-03-06 11:25:55 -0800

    Updating link for langchain website

--- main.py
+++ main.py
@@ -52,7 +52,7 @@ col1, col2 = st.columns(2)
 with col1:
     st.markdown("Often professionals would like to improve their emails, but don't have the skills to do so. \n\n This tool \
                 will help you improve your email skills by converting your emails into a more professional format. This tool \
-                is powered by [LangChain](www.langchain.com) and [OpenAI](https://openai.com) and made by \
+                is powered by [LangChain](https://langchain.com/) and [OpenAI](https://openai.com) and made by \
                 [@GregKamradt](https://twitter.com/GregKamradt). \n\n View Source Code on [Github](https://github.com/gkamradt/globalize-text-streamlit/blob/main/main.py)")
 
 with col2:
