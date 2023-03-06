a5ffa7af67dfd725c5199d8482ab200d1eee8959
Gregory Kamradt <gregorykamradt@Gregs-M1.local>
2023-03-05 16:12:18 -0800

    polish

--- README.md
+++ README.md
@@ -2,9 +2,12 @@
 
 This repo serves as a template for how to make your own LangChain apps on Streamlit.
 
-Video of how this code was made: 
+View Live App: https://globalize.streamlit.app/
+Video of how this code was made: https://www.youtube.com/watch?v=U_eV8wfMkXU
+
+![Email Converter Preview](https://i.imgur.com/4vHwjId.png)
 
 ## Deploy on Streamlit
 
-This is easily deployable on the Streamlit platform.
+This app is meant to be deployed on [Streamlit](https://streamlit.io/).
 Note that when setting up your StreamLit app you should make sure to add `OPENAI_API_KEY` as a secret environment variable.
--- main.py
+++ main.py
@@ -14,15 +14,21 @@ template = """
     - Informal: Went to Barcelona for the weekend. Lots to tell you.  
 
     Here are some examples of words in different dialects:
-    - American English: French Fries, cotton candy, apartment, garbage, cookie, green thumb, parking lot, pants, windshield
-    - British English: chips, candyfloss, flag, rubbish, biscuit, green fingers, car park, trousers, windscreen
+    - American: French Fries, cotton candy, apartment, garbage, cookie, green thumb, parking lot, pants, windshield
+    - British: chips, candyfloss, flag, rubbish, biscuit, green fingers, car park, trousers, windscreen
 
+    Example Sentences from each dialect:
+    - American: I headed straight for the produce section to grab some fresh vegetables, like bell peppers and zucchini. After that, I made my way to the meat department to pick up some chicken breasts.
+    - British: Well, I popped down to the local shop just the other day to pick up a few bits and bobs. As I was perusing the aisles, I noticed that they were fresh out of biscuits, which was a bit of a disappointment, as I do love a good cuppa with a biscuit or two.
+
+    Please start the email with a warm introduction. Add the introduction if you need to.
+    
     Below is the email, tone, and dialect:
     TONE: {tone}
     DIALECT: {dialect}
     EMAIL: {email}
     
-    YOUR RESPONSE:
+    YOUR {dialect} RESPONSE:
 """
 
 prompt = PromptTemplate(
@@ -32,7 +38,8 @@ prompt = PromptTemplate(
 
 def load_LLM():
     """Logic for loading the chain you want to use should go here."""
-    llm = OpenAI(temperature=.5)
+    # Make sure your openai_api_key is set as an environment variable
+    llm = OpenAI(temperature=.7)
     return llm
 
 llm = load_LLM()
@@ -45,12 +52,15 @@ col1, col2 = st.columns(2)
 with col1:
     st.markdown("Often professionals would like to improve their emails, but don't have the skills to do so. \n\n This tool \
                 will help you improve your email skills by converting your emails into a more professional format. This tool \
-                is powered by [LangChain](www.langchain.com) and [OpenAI](https://openai.com) and made by [@GregKamradt](https://twitter.com/GregKamradt).")
+                is powered by [LangChain](www.langchain.com) and [OpenAI](https://openai.com) and made by \
+                [@GregKamradt](https://twitter.com/GregKamradt). \n\n View Source Code on [Github](https://github.com/gkamradt/globalize-text-streamlit/blob/main/main.py)")
 
 with col2:
     st.image(image='TweetScreenshot.png', width=500, caption='https://twitter.com/DannyRichman/status/1598254671591723008')
 
 st.markdown("## Enter Your Email To Convert")
+
+
 
 col1, col2 = st.columns(2)
 with col1:
@@ -61,13 +71,23 @@ with col1:
 with col2:
     option_dialect = st.selectbox(
         'Which English Dialect would you like?',
-        ('American English', 'British English'))
+        ('American', 'British'))
 
 def get_text():
-    input_text = st.text_area(label="", placeholder="Your Email...", key="email_input")
+    input_text = st.text_area(label="Email Input", label_visibility='collapsed', placeholder="Your Email...", key="email_input")
     return input_text
 
 email_input = get_text()
+
+if len(email_input.split(" ")) > 700:
+    st.write("Please enter a shorter email. The maximum length is 700 words.")
+    st.stop()
+
+def update_text_with_example():
+    print ("in updated")
+    st.session_state.email_input = "Sally I am starts work at yours monday from dave"
+
+st.button("*See An Example*", type='secondary', help="Click to see an example of the email you will be converting.", on_click=update_text_with_example)
 
 st.markdown("### Your Converted Email:")
 
@@ -77,3 +97,4 @@ if email_input:
     formatted_email = llm(prompt_with_email)
 
     st.write(formatted_email)
+    
