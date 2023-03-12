e3d9c69b4e55047a393d76ac5a2098f537f8560a
Prithvi Tharun <ptrithu8@gmail.com>
2023-03-12 13:17:17 +0530

    Hero section added

--- main.py
+++ main.py
@@ -42,21 +42,14 @@ def load_LLM(openai_api_key):
     llm = OpenAI(temperature=.7, openai_api_key=openai_api_key)
     return llm
 
-st.set_page_config(page_title="Globalize Email", page_icon=":robot:")
-st.header("Globalize Text")
+st.set_page_config(page_title="Better Grammar Ally", layout="wide")
 
-col1, col2 = st.columns(2)
+st.markdown("# Better thoughts with AI ✨")
 
-with col1:
-    st.markdown("Often professionals would like to improve their emails, but don't have the skills to do so. \n\n This tool \
-                will help you improve your email skills by converting your emails into a more professional format. This tool \
-                is powered by [LangChain](https://langchain.com/) and [OpenAI](https://openai.com) and made by \
-                [@GregKamradt](https://twitter.com/GregKamradt). \n\n View Source Code on [Github](https://github.com/gkamradt/globalize-text-streamlit/blob/main/main.py)")
+with st.container():
+   st.markdown("Made by Prithvi")
 
-with col2:
-    st.image(image='TweetScreenshot.png', width=500, caption='https://twitter.com/DannyRichman/status/1598254671591723008')
-
-st.markdown("## Enter Your Email To Convert")
+st.markdown("### You thoughts")
 
 def get_api_key():
     input_text = st.text_input(label="OpenAI API Key ",  placeholder="Ex: sk-2twmA8tfCb8un4...", key="openai_api_key_input")
